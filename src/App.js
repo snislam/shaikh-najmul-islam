3245823a88ef79b7481519f7135af699121407d5
Shaikh Najmul Islam <najmulbge17@gmail.com>
2022-06-09 19:23:03 +0600

    Menu hover and responsive done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Contact from './Pages/Contact/Contact';
+import Home from './Pages/Home/Home';
 import Navbar from './shared/Navbar';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
     </>
   );
 }
--- src/shared/Navbar.js
+++ src/shared/Navbar.js
@@ -13,13 +13,13 @@ const Navbar = () => {
                     </Link>
                 </div>
                 <div>
-                    <button className='block md:hidden' onClick={() => setIsOpen(!isOpen)}> {!isOpen ? <MenuIcon className='w-8 h-8' /> : <XIcon className='w-8 h-8' />} </button>
+                    <button className='block md:hidden' onClick={() => setIsOpen(!isOpen)}> {!isOpen ? <MenuIcon className='w-8 h-8 text-white' /> : <XIcon className='w-8 h-8 text-white' />} </button>
                     <menu className={isOpen ? 'absolute md:static md:mt-0 left-0 top-[70px] w-full px-8 py-3 duration-700 md:duration-0 bg-[#FBD062] md:bg-transparent' : 'absolute md:static mt-[-580px] md:mt-0 left-0 top-[70px] w-full px-8 py-3 duration-700 md:duration-0 bg-[#FBD062] md:bg-transparent'}>
                         <ul className='block md:flex md:flex-row justify-center items-center'>
-                            <li className='my-2 py-1 bg-[#f9c43d] md:bg-transparent hover:bg-yellow-600 font-semibold hover:border-b-2 border-[#d09809]'><Link className='text-slate-200 px-1 mx-2 py-1 focus:bg-[#111430] focus:text-white duration-300 rounded-sm' to='/'>Home</Link></li>
-                            <li className='my-2 py-1 bg-[#f9c43d] md:bg-transparent hover:bg-yellow-600 font-semibold hover:border-b-2 border-[#d09809]'><Link className='text-slate-200 px-1 mx-2 py-1 focus:bg-[#111430] focus:text-white duration-300 rounded-sm' to='#portfolio'>Our Portfolio</Link></li>
-                            <li className='my-2 py-1 bg-[#f9c43d] md:bg-transparent hover:bg-yellow-600 font-semibold hover:border-b-2 border-[#d09809]'><Link className='text-slate-200 px-1 mx-2 py-1 focus:bg-[#111430] focus:text-white duration-300 rounded-sm' to='#our-team'>Our Team</Link></li>
-                            <li className='my-2 py-1 bg-[#f9c43d] md:bg-transparent hover:bg-yellow-600 font-semibold hover:border-b-2 border-[#d09809]'><Link className='text-slate-200 px-1 mx-2 py-1 focus:bg-[#111430] focus:text-white duration-300 rounded-sm' to='#contact-us'>Contact Us</Link></li>
+                            <li className='my-2 py-1 bg-[#f9c43d] md:bg-transparent hover:bg-yellow-600 duration-500 font-semibold hover:border-b-2 border-[#d09809]'><Link className='text-slate-200 px-2 py-2 focus:bg-[#111430] focus:text-white duration-300 rounded-sm' to='/'>Home</Link></li>
+                            <li className='my-2 py-1 bg-[#f9c43d] md:bg-transparent hover:bg-yellow-600 duration-500 font-semibold hover:border-b-2 border-[#d09809]'><Link className='text-slate-200 px-2 py-2 focus:bg-[#111430] focus:text-white duration-300 rounded-sm' to='#portfolio'>Our Portfolio</Link></li>
+                            <li className='my-2 py-1 bg-[#f9c43d] md:bg-transparent hover:bg-yellow-600 duration-500 font-semibold hover:border-b-2 border-[#d09809]'><Link className='text-slate-200 px-2 py-2 focus:bg-[#111430] focus:text-white duration-300 rounded-sm' to='#our-team'>Our Team</Link></li>
+                            <li className='my-2 py-1 bg-[#f9c43d] md:bg-transparent hover:bg-yellow-600 duration-500 font-semibold hover:border-b-2 border-[#d09809]'><Link className='text-slate-200 px-2 py-2 focus:bg-[#111430] focus:text-white duration-300 rounded-sm' to='/contact'>Contact Us</Link></li>
                         </ul>
                     </menu>
                 </div>
